47dbfa4f8e5682fc7461be724aef83a0c4484136
Dhimantjadeja <dhimant.inexture@gmail.com>
2017-12-08 16:39:20 +0530

    adding EnvironmentSetup

--- ValidateInstall.ps1
+++ ValidateInstall.ps1
@@ -1,9 +1,2 @@
-﻿sl C:\ExploringAspNetCore\SampleApp\src\SmokeTests
-
-# Restore the nuget references
-& "C:\Program Files\dotnet\dotnet.exe" restore
-
-# Run the smoke tests
-& "C:\Program Files\dotnet\dotnet.exe" test
-
-exit $LastExitCode
+﻿$strString = "Hello World"
+write-host $strString
