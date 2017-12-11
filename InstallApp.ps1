3d8db90bc89dffbc9d434c96bbe990a43a99f7e1
Dhimantjadeja <dhimant.inexture@gmail.com>
2017-12-11 12:02:33 +0530

    ading Installapp file

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -1,2 +1,12 @@
-$strString = "Hello World"
-write-host $strString
+﻿sl C:\inetpub\wwwroot
+
+# Restore the nuget references
+& "C:\Program Files\dotnet\dotnet.exe" restore
+
+# Publish application with all of its dependencies and runtime for IIS to use
+& "C:\Program Files\dotnet\dotnet.exe" publish --configuration release -o C:\inetpub\wwwroot --runtime active
+
+
+# Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
+# To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
+C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value C:\inetpub\wwwroot}
